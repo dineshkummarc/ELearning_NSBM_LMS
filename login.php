178b626057e2d024e15db987b6546c1b8b67335f
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:48:15 +0530

    update

--- login.php
+++ login.php
@@ -5,13 +5,13 @@
 		$password = $_POST['password'];
 		/* student */
 			$query = "SELECT * FROM student WHERE username='$username' AND password='$password'";
-			$result = mysql_query($query)or die(mysql_error());
-			$row = mysql_fetch_array($result);
-			$num_row = mysql_num_rows($result);
+			$result = mysqli_query($GLOBALS["___mysqli_ston"], $query)or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+			$row = mysqli_fetch_array($result);
+			$num_row = mysqli_num_rows($result);
 		/* teacher */
-		$query_teacher = mysql_query("SELECT * FROM teacher WHERE username='$username' AND password='$password'")or die(mysql_error());
-		$num_row_teacher = mysql_num_rows($query_teacher);
-		$row_teahcer = mysql_fetch_array($query_teacher);
+		$query_teacher = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM teacher WHERE username='$username' AND password='$password'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+		$num_row_teacher = mysqli_num_rows($query_teacher);
+		$row_teahcer = mysqli_fetch_array($query_teacher);
 		if( $num_row > 0 ) { 
 		$_SESSION['id']=$row['student_id'];
 		echo 'true_student';	
