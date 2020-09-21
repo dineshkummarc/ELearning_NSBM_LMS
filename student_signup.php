a49c15e3fad4121c702a5d8c94bec269ed5088c0
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:45:51 +0530

    update

--- student_signup.php
+++ student_signup.php
@@ -7,13 +7,13 @@ $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $class_id = $_POST['class_id'];
 
-$query = mysql_query("select * from student where username='$username' and firstname='$firstname' and lastname='$lastname' and class_id = '$class_id'")or die(mysql_error());
-$row = mysql_fetch_array($query);
+$query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from student where username='$username' and firstname='$firstname' and lastname='$lastname' and class_id = '$class_id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+$row = mysqli_fetch_array($query);
 $id = $row['student_id'];
 
-$count = mysql_num_rows($query);
+$count = mysqli_num_rows($query);
 if ($count > 0){
-	mysql_query("update student set password = '$password', status = 'Registered' where student_id = '$id'")or die(mysql_error());
+	mysqli_query($GLOBALS["___mysqli_ston"], "update student set password = '$password', status = 'Registered' where student_id = '$id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 	$_SESSION['id']=$id;
 	echo 'true';
 }else{
