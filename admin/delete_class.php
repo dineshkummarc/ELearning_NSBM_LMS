1425eb34f1d342adf8a9c4a900cf77ff5e150bd2
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:56:09 +0530

    update

--- admin/delete_class.php
+++ admin/delete_class.php
@@ -5,7 +5,7 @@ $id=$_POST['selector'];
 $N = count($id);
 for($i=0; $i < $N; $i++)
 {
-	$result = mysql_query("DELETE FROM class where class_id='$id[$i]'");
+	$result = mysqli_query($GLOBALS["___mysqli_ston"], "DELETE FROM class where class_id='$id[$i]'");
 }
 header("location: class.php");
 }
