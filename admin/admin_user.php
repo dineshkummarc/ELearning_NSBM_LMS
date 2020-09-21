9912ffc941316c82a95f49bce689bed1744ca733
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:55:31 +0530

    update

--- admin/admin_user.php
+++ admin/admin_user.php
@@ -32,8 +32,8 @@
 										</thead>
 										<tbody>
 													<?php
-													$user_query = mysql_query("select * from users")or die(mysql_error());
-													while($row = mysql_fetch_array($user_query)){
+													$user_query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from users")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+													while($row = mysqli_fetch_array($user_query)){
 													$id = $row['user_id'];
 													?>
 									
