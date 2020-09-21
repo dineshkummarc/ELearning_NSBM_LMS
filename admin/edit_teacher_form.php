b1ecd4afcd7689c75edffd50c4417446b7186f65
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:53:28 +0530

    update

--- admin/edit_teacher_form.php
+++ admin/edit_teacher_form.php
@@ -17,8 +17,8 @@
                                         </div>
 									-->	
 									<?php
-									$query = mysql_query("select * from teacher where teacher_id = '$get_id' ")or die(mysql_error());
-									$row = mysql_fetch_array($query);
+									$query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from teacher where teacher_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+									$row = mysqli_fetch_array($query);
 									?>
 										
 										  <div class="control-group">
@@ -26,8 +26,8 @@
                                           <div class="controls">
                                             <select name="department"  class="chzn-select"required>
 											<?php
-											$query_teacher = mysql_query("select * from teacher join  department")or die(mysql_error());
-											$row_teacher = mysql_fetch_array($query_teacher);
+											$query_teacher = mysqli_query($GLOBALS["___mysqli_ston"], "select * from teacher join  department")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+											$row_teacher = mysqli_fetch_array($query_teacher);
 											
 											?>
 											
@@ -35,8 +35,8 @@
 												<?php echo $row_teacher['department_name']; ?>
 												</option>
 											<?php
-											$department = mysql_query("select * from department order by department_name");
-											while($department_row = mysql_fetch_array($department)){
+											$department = mysqli_query($GLOBALS["___mysqli_ston"], "select * from department order by department_name");
+											while($department_row = mysqli_fetch_array($department)){
 											
 											?>
 											<option value="<?php echo $department_row['department_id']; ?>"><?php echo $department_row['department_name']; ?></option>
@@ -81,8 +81,8 @@
                                 $department_id = $_POST['department'];
 								
 								
-								$query = mysql_query("select * from teacher where firstname = '$firstname' and lastname = '$lastname' ")or die(mysql_error());
-								$count = mysql_num_rows($query);
+								$query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from teacher where firstname = '$firstname' and lastname = '$lastname' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+								$count = mysqli_num_rows($query);
 								
 								if ($count > 1){ ?>
 								<script>
@@ -91,7 +91,7 @@
 								<?php
 								}else{
 								
-								mysql_query("update teacher set firstname = '$firstname' , lastname = '$lastname' , department_id = '$department_id' where teacher_id = '$get_id' ")or die(mysql_error());	
+								mysqli_query($GLOBALS["___mysqli_ston"], "update teacher set firstname = '$firstname' , lastname = '$lastname' , department_id = '$department_id' where teacher_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));	
 								
 								?>
 								<script>
