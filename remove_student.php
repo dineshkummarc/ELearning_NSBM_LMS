706276503714e6b322467b6ede3c658a2f51e310
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:46:46 +0530

    update

--- remove_student.php
+++ remove_student.php
@@ -1,6 +1,6 @@
 <?php
 include('admin/dbcon.php');
 $id = $_POST['id'];
-mysql_query("delete from teacher_class_student where teacher_class_student_id = '$id'")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from teacher_class_student where teacher_class_student_id = '$id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 ?>
 
