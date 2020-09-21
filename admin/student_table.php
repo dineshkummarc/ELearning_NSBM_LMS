dc1a24ea88e1733549f179779da309f11df0c920
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:52:26 +0530

    update

--- admin/student_table.php
+++ admin/student_table.php
@@ -30,8 +30,8 @@
 		<tbody>
 			
 		<?php
-	$query = mysql_query("select * from student LEFT JOIN class ON student.class_id = class.class_id ORDER BY student.student_id DESC") or die(mysql_error());
-	while ($row = mysql_fetch_array($query)) {
+	$query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from student LEFT JOIN class ON student.class_id = class.class_id ORDER BY student.student_id DESC") or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+	while ($row = mysqli_fetch_array($query)) {
 		$id = $row['student_id'];
 		?>
 	
