be9fb04d49c3af17a00d8ffb7aec632dffb805af
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:56:27 +0530

    update

--- admin/delete_student.php
+++ admin/delete_student.php
@@ -5,8 +5,8 @@ $id=$_POST['selector'];
 $N = count($id);
 for($i=0; $i < $N; $i++)
 {
-	 mysql_query("DELETE FROM student where student_id='$id[$i]'");
-	 mysql_query("DELETE FROM teacher_class_student where student_id='$id[$i]'");
+	 mysqli_query($GLOBALS["___mysqli_ston"], "DELETE FROM student where student_id='$id[$i]'");
+	 mysqli_query($GLOBALS["___mysqli_ston"], "DELETE FROM teacher_class_student where student_id='$id[$i]'");
 }
 header("location: students.php");
 }
