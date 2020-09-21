edc15da41a333e652fa8a58780ac30e2de9c0ce5
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:53:07 +0530

    update

--- admin/login.php
+++ admin/login.php
@@ -4,9 +4,9 @@
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-		$query = mysql_query("SELECT * FROM users WHERE username='$username' AND password='$password'")or die(mysql_error());
-		$count = mysql_num_rows($query);
-		$row = mysql_fetch_array($query);
+		$query = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM users WHERE username='$username' AND password='$password'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+		$count = mysqli_num_rows($query);
+		$row = mysqli_fetch_array($query);
 
 
 		if ($count > 0){
@@ -15,7 +15,7 @@
 		
 		echo 'true';
 		
-		mysql_query("insert into user_log (username,login_date,user_id)values('$username',NOW(),".$row['user_id'].")")or die(mysql_error());
+		mysqli_query($GLOBALS["___mysqli_ston"], "insert into user_log (username,login_date,user_id)values('$username',NOW(),".$row['user_id'].")")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 		 }else{ 
 		echo 'false';
 		}	
