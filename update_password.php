5b95be0051309568f1ddbf46310352aa8a373f8e
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:44:39 +0530

    update

--- update_password.php
+++ update_password.php
@@ -2,5 +2,5 @@
  include('dbcon.php');
  include('session.php');
  $new_password  = $_POST['new_password'];
- mysql_query("update teacher set password = '$new_password' where teacher_id = '$session_id'")or die(mysql_error());
+ mysqli_query($GLOBALS["___mysqli_ston"], "update teacher set password = '$new_password' where teacher_id = '$session_id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
  ?>
