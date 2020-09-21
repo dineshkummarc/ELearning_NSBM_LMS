60e62a3141b87b9526ff573c0619cff210c8121a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:56:22 +0530

    update

--- admin/delete_event.php
+++ admin/delete_event.php
@@ -1,7 +1,7 @@
 <?php
 include('dbcon.php');
 $get_id = $_GET['id'];
-mysql_query("delete from event where event_id = '$get_id'")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from event where event_id = '$get_id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 ?>
 <script>
 window.location = 'calendar_of_events.php';
