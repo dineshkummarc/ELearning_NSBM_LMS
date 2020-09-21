17b382b7f6184f3b215f03057bc4d14819ecd08a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:49:12 +0530

    update

--- delete_file.php
+++ delete_file.php
@@ -1,5 +1,5 @@
 <?php
 include('admin/dbcon.php');
 $id = $_POST['id'];
-mysql_query("delete from files where file_id = '$id' ")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from files where file_id = '$id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 ?>
