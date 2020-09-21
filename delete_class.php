0576407b59a6961b6aec199b7deb25b888d445de
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:49:25 +0530

    update

--- delete_class.php
+++ delete_class.php
@@ -1,10 +1,10 @@
 <?php
 include('admin/dbcon.php');
 $get_id = $_POST['id'];
-mysql_query("delete from teacher_class  where  teacher_class_id = '$get_id' ")or die(mysql_error());
-mysql_query("delete from teacher_class_student  where  teacher_class_id = '$get_id' ")or die(mysql_error());
-mysql_query("delete from teacher_class_announcements  where  teacher_class_id = '$get_id' ")or die(mysql_error());
-mysql_query("delete from teacher_notification  where  teacher_class_id = '$get_id' ")or die(mysql_error());
-mysql_query("delete from class_subject_overview where  teacher_class_id = '$get_id' ")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from teacher_class  where  teacher_class_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from teacher_class_student  where  teacher_class_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from teacher_class_announcements  where  teacher_class_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from teacher_notification  where  teacher_class_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from class_subject_overview where  teacher_class_id = '$get_id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 header('location:dasboard_teacher.php');
 ?>
