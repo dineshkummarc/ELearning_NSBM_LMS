a1c2102fb81d812a4674982030591aaf76da64b2
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 11:54:48 +0530

    update

--- admin/add_school_year.php
+++ admin/add_school_year.php
@@ -33,8 +33,8 @@ $school_year = $_POST['school_year'];
 
 
 
-$query = mysql_query("select * from school_year where school_year = '$school_year'")or die(mysql_error());
-$count = mysql_num_rows($query);
+$query = mysqli_query($GLOBALS["___mysqli_ston"], "select * from school_year where school_year = '$school_year'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+$count = mysqli_num_rows($query);
 
 if ($count > 0){ ?>
 <script>
@@ -42,9 +42,9 @@ alert('Data Already Exist');
 </script>
 <?php
 }else{
-mysql_query("insert into school_year (school_year) values('$school_year')")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "insert into school_year (school_year) values('$school_year')")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 
-mysql_query("insert into activity_log (date,username,action) values(NOW(),'$user_username','Add Academic Year $school_year')")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "insert into activity_log (date,username,action) values(NOW(),'$user_username','Add Academic Year $school_year')")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 ?>
 <script>
 window.location = "school_year.php";
